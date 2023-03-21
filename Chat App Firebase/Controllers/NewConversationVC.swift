976d2453bd4b28405d82502268cdb0b1b1ff34f5
Rustam Manafli <34415394+rustemmanafov@users.noreply.github.com>
2023-03-21 22:01:30 +0400

    updated

---
+++ Chat App Firebase/Controllers/NewConversationVC.swift
@@ -0,0 +1,21 @@
+//
+//  NewConversationVC.swift
+//  Chat App Firebase
+//
+//  Created by Rustem Manafov on 21.03.23.
+//
+
+import UIKit
+
+class NewConversationVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+
+}
